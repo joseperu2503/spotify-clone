cc5754c86833c939a0ef89384ab178d1846e86a0
joseperu2503 <51133263+joseperu2503@users.noreply.github.com>
2022-11-09 22:40:47 -0500

    song

--- components/Center.js
+++ components/Center.js
@@ -1,7 +1,11 @@
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
-import { useSession } from 'next-auth/react'
+import { useSession, signOut } from 'next-auth/react'
 import { shuffle } from 'lodash'
 import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { playlistIdState, playlistState } from '../atoms/playlistAtom'
+import useSpotify from '../hooks/useSpotify'
+import Songs from './Songs'
 
 const colors = [
   'from-indigo-500',
@@ -16,25 +20,53 @@ const colors = [
 const Center = () => {
 
   const { data: session } = useSession()
+  const spotifyApi = useSpotify()
   const [color, setColor] = useState(null);
+  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState)
+  const [playlist, setPlaylist] = useRecoilState(playlistState)
+
+  console.log('playlistId2',playlistId)
 
   useEffect(() => {
     setColor(shuffle(colors).pop())
-  }, []);
+  }, [playlistId]);
+
+  useEffect(() => {
+    console.log('cargando playlist',spotifyApi.getAccessToken())
+    if(spotifyApi.getAccessToken()){
+      console.log('hay token')
+      spotifyApi.getPlaylist(playlistId)
+      .then(data => {
+        setPlaylist(data.body)
+      })
+      .catch(error => console.log('error al cargar la playlist ',error))
+
+    }
+  }, [spotifyApi, playlistId]);
+  console.log('playlist',playlist)
 
   return (
-    <div className='flex-grow text-white'>
+    <div className='flex-grow text-white h-screen overflow-y-scroll'>
       <header className='absolute top-5 right-8'>
-        <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 '>
+        <div
+          className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'
+          onClick={() => signOut()}
+        >
           <img className='rounded-full w-10 h-10' src={session?.user.image} alt="" />
           <h2>{session?.user.name}</h2>
           <ChevronDownIcon className='h-5 w-5'/>
         </div>
       </header>
       <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8 w-full`}>
-        <img src="" alt="" />
-        <h1>hello</h1>
+        <img src={playlist?.images?.[0]?.url} alt="" className='h-44 w-44 shadow-2xl'/>
+        <div>
+          <p>PLAYLIST</p>
+          <h1 className='text-2xl md:text-3xl xl:text-5xl font-bold'>{playlist?.name}</h1>
+        </div>
       </section>
+      <div>
+        <Songs/>
+      </div>
     </div>
   )
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -10,30 +10,35 @@ import {
 import { signOut, useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import useSpotify from '../hooks/useSpotify'
+import { useRecoilState } from 'recoil'
+import { playlistIdState } from '../atoms/playlistAtom'
+
+
 
 function Sidebar() {
 
   const spotifyApi = useSpotify()
   const {data: session, status } = useSession()
   const [playlists, setPlaylists] = useState([])
+  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState)
+
+  console.log('ypu picked playlist >> ', playlistId)
 
   useEffect(() => {
     if(spotifyApi.getAccessToken()){
-      spotifyApi.getUserPlaylists().then((data) => {
+      spotifyApi.getUserPlaylists()
+      .then((data) => {
         setPlaylists(data.body.items)
       })
+      .catch(error => console.log(error))
     }
   }, [session, spotifyApi]);
 
   console.log(playlists)
 
   return (
-    <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen'>
+    <div className='text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex'>
       <div className='space-y-4'>
-        <button className='flex items-center space-x-2 hover:text-white' onClick={() => signOut()}>
-          <HomeIcon className="h-5 w-5"/>
-          <p>Logout</p>
-        </button>
         <button className='flex items-center space-x-2 hover:text-white'>
           <HomeIcon className="h-5 w-5"/>
           <p>Home</p>
@@ -63,7 +68,13 @@ function Sidebar() {
         <hr className='border-t-[0.1px] border-gray-900'/>
         {/* Playlists */}
         { playlists.map( playlist => (
-          <p key={playlist.id} className='cursor-pointer hover:text-white'>{playlist.name}</p>
+          <p
+           key={playlist.id}
+           className='cursor-pointer hover:text-white'
+           onClick={() => setPlaylistId(playlist.id)}
+          >
+            {playlist.name}
+          </p>
         ))}
       </div>
     </div>
